c8c6010da7964a122b4b944f40feeaa1e8f5faf7
Matt Ruffoni <m.ruffoni@imperial.ac.uk>
2014-06-04 16:40:58 +0100

    Added ability to shade graph objects based on calibration status

--- src/graph.cpp
+++ src/graph.cpp
@@ -31,6 +31,7 @@ Graph::Graph () {
   GraphMax.x = 0.0; GraphMax.y = 0.0;
   Selected = false;
   Disabled = false;
+  Calibration = false;
   AutoLimits = false;
 }
 
@@ -40,6 +41,7 @@ Graph::Graph (XgLine LineIn, vector <Coord> Points) {
   GraphMax.x = 0.0; GraphMax.y = 0.0;
   Selected = false;
   Disabled = false;
+  Calibration = false;
   AutoLimits = false;
   addPlot (LineIn, Points);
 }
@@ -78,9 +80,11 @@ bool Graph::on_expose_event(GdkEventExpose* event)
       width - TOTAL_X_PAD, height - TOTAL_Y_PAD);
     if (Selected) {
       if (Disabled) cr -> set_source_rgb(COLOUR_DIS_SEL);
+      if (Calibration) cr -> set_source_rgb(COLOUR_SEL_CAL);
       else cr -> set_source_rgb(COLOUR_SELECTED);
     } else {
       if (Disabled) cr -> set_source_rgb(COLOUR_DISABLED);
+      if (Calibration) cr -> set_source_rgb(COLOUR_SELECTED);
       else cr -> set_source_rgb(COLOUR_BACK);
     }
     cr->fill_preserve ();
